7b8d930ac4bdca523ebc74024c02e05beb641fc8
Fernando Chen <fernando.lichen@outlook.com>
2024-03-07 13:49:21 -0600

    Added navbar and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Navbar'
+import Example from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="text-sky-400">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Example />
   );
 }
 
