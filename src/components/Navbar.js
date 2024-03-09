8f59fb257bd6e236cc49ed37ce620073d3d08e34
Fernando Chen <fernando.lichen@outlook.com>
2024-03-08 21:56:08 -0600

    Changed logo image and remove the old one

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import logo from '../images/Wizeline_Wordmark.png';
+import logo from '../images/wizeline_logo.png';
 
 const navigation = [
   { name: 'Work', href: '/#work', current: true },
