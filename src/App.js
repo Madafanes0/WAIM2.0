04d34f33fea5d255dcfad9d5bbdeb86c5a8fd91b
Madafanes0 <dcisneros0206@gmail.com>
2024-03-09 21:28:09 -0600

    Router Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import {OrbitControls} from '@react-three/drei';
-import './components/Navbar'
+import { OrbitControls } from '@react-three/drei';
 import NavbarW from './components/Navbar';
 import ClickableBox from './components/ClickableBox';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AnotherPage from './components/AnotherPage';
 
 export default function App() {
   return (
-    
-    <>
-      <NavbarW />
-      <Canvas className="canvas" style={{height:'700px'}} >
-        <OrbitControls enableZoom={false} enablePan={false}/>
-        
-        <ClickableBox />
-      </Canvas>
-    </>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<>
+          <NavbarW />
+          <Canvas className="canvas" style={{ height: '700px' }}>
+            <OrbitControls enableZoom={false} enablePan={false} />
+            <ClickableBox />
+          </Canvas>
+        </>} />
+        <Route path="/another-page" element={<AnotherPage />} />
+      </Routes>
+    </Router>
   );
 }
-
---
+++ src/components/AnotherPage.js
@@ -0,0 +1,5 @@
+const AnotherPage = () => {
+    return <h1>Home</h1>;
+  };
+  
+  export default AnotherPage;
